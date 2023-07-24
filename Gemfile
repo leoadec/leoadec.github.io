f7ec0882097245ea8b95c2f2a5333b2b1c665fc9
leoadec@gmail.com <leoadec@gmail.com>
2023-07-24 23:03:25 +1000

    add new apps

--- Gemfile
+++ Gemfile
@@ -5,5 +5,4 @@ source "https://rubygems.org"
 # gem "rails"
 
 gem "jekyll", "~> 4.3"
-
 gem "rake", "~> 13.0"
---
+++ assets/js/utf8Conversion.js
@@ -0,0 +1,37 @@
+const brazilianPlaceNames = new Map([
+    ["Amapa", "Amapá"],
+    ["Belem", "Belém"],
+    ["Brasilia", "Brasília"],
+    ["Ceara", "Ceará"],
+    ["Cuiaba", "Cuiabá"],
+    ["Florianopolis", "Florianópolis"],
+    ["Goiania", "Goiânia"],
+    ["Goias", "Goiás"],
+    ["Joao Pessoa", "João Pessoa"],
+    ["Macapa", "Macapá"],
+    ["Maceio", "Maceió"],
+    ["Maranhao", "Maranhão"],
+    ["Para", "Pará"],
+    ["Paraiba", "Paraíba"],
+    ["Piauí", "Piaui"],
+    ["Rondonia", "Rondônia"],
+    ["Sao Luis", "São Luís"],
+    ["Sao Paulo", "São Paulo"],
+    ["Vitoria", "Vitória"],
+])
+
+
+function convertWord(word) {
+    if (brazilianPlaceNames.has(word)) {
+        return brazilianPlaceNames.get(word);
+    };
+    return word;
+}
+
+function utf8Conversion(text) {
+    const words = text.trim().replace(/ +/g, " ").split(" ");
+
+    let updatedWords = words.map(convertWord);
+
+    return updatedWords.join(" ");
+}
