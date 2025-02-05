bc77e1dd13b8cae4a36f0d3c545fb6b4c9591ae3
Leonardo Andreta de Castro <leonardo.andreta@q-ctrl.com>
2025-02-06 10:16:32 +1100

    update gitignore with vim files

--- crates/chip8/src/lib.rs
+++ crates/chip8/src/lib.rs
@@ -1,41 +1,9 @@
+mod stack;
+
+use stack::Stack;
+
 pub const SCREEN_HEIGHT: usize = 32;
 pub const SCREEN_WIDTH: usize = 64;
-
-const STACK_SIZE: u8 = 16;
-
-#[derive(Debug)]
-struct Stack {
-    values: [u16; STACK_SIZE as usize],
-    pointer: u8,
-}
-
-impl Stack {
-    fn new() -> Self {
-        Stack {
-            values: [0; STACK_SIZE as usize],
-            pointer: 0,
-        }
-    }
-
-    fn push(&mut self, value: u16) {
-        if self.pointer >= (STACK_SIZE - 1) {
-            panic!("Trying to push to a full stack.");
-        }
-
-        self.values[self.pointer as usize] = value;
-        self.pointer += 1;
-    }
-
-    fn pop(&mut self) -> u16 {
-        if self.pointer <= 0 {
-            panic!("Trying to push to a full stack.");
-        }
-
-        self.pointer -= 1;
-
-        self.values[self.pointer as usize]
-    }
-}
 
 #[derive(Debug)]
 pub struct Chip8 {
