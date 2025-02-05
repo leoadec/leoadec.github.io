3a72c404b593222edbc2470e54e758b54a479b4f
Leonardo Andreta de Castro <leonardo.andreta@q-ctrl.com>
2025-02-06 10:17:01 +1100

    move stack struct to a separate file

---
+++ crates/chip8/src/stack.rs
@@ -0,0 +1,35 @@
+const STACK_SIZE: u8 = 16;
+
+#[derive(Debug)]
+pub struct Stack {
+    values: [u16; STACK_SIZE as usize],
+    pointer: u8,
+}
+
+impl Stack {
+    pub fn new() -> Self {
+        Stack {
+            values: [0; STACK_SIZE as usize],
+            pointer: 0,
+        }
+    }
+
+    pub fn push(&mut self, value: u16) {
+        if self.pointer >= (STACK_SIZE - 1) {
+            panic!("Trying to push to a full stack.");
+        }
+
+        self.values[self.pointer as usize] = value;
+        self.pointer += 1;
+    }
+
+    pub fn pop(&mut self) -> u16 {
+        if self.pointer <= 0 {
+            panic!("Trying to push to a full stack.");
+        }
+
+        self.pointer -= 1;
+
+        self.values[self.pointer as usize]
+    }
+}
