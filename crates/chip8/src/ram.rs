906314d8760e231b19406a8b3007937d0ce77eea
Leonardo Andreta de Castro <leonardo.andreta@q-ctrl.com>
2025-02-06 11:39:00 +1100

    include fetch method for reading RAM

--- crates/chip8/src/ram.rs
+++ crates/chip8/src/ram.rs
@@ -2,6 +2,7 @@ const RAM_SIZE: usize = 4 * 1024;
 
 #[derive(Debug)]
 pub struct Ram {
+    program_counter: u8,
     memory: [u8; RAM_SIZE],
 }
 
@@ -9,6 +10,20 @@ impl Ram {
     pub fn new() -> Self {
         Ram {
             memory: [0; RAM_SIZE],
+            program_counter: 0,
         }
+    }
+
+    pub fn fetch(&mut self) -> u16 {
+        if (self.program_counter as usize) >= (RAM_SIZE - 1) {
+            panic!("Attempt to read beyond the size of the RAM.");
+        };
+
+        let first_byte = self.memory[self.program_counter as usize] as u16;
+        let second_byte = self.memory[self.program_counter as usize + 1] as u16;
+
+        self.program_counter += 2;
+
+        (first_byte << 8) | second_byte
     }
 }
