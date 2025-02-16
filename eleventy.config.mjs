852a62fd51a29915100140c97364a930cc573c53
Leo Castro <leoadec@gmail.com>
2025-02-16 20:28:35 +1100

    add post, resolve conflicts

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,9 +1,23 @@
+import mathjax from 'mathjax';
 import * as sass from 'sass';
+
+const MathJax = await mathjax.init({
+    loader: { load: ['input/tex', 'output/svg'] },
+})
 
 export default async function(eleventyConfig) {
     eleventyConfig.setInputDirectory('_templates');
     eleventyConfig.setLayoutsDirectory('_layouts');
     eleventyConfig.setOutputDirectory('_site');
+
+    eleventyConfig.addTemplateFormats('mathjax');
+    eleventyConfig.addExtension('mathjax', {
+      outputFileExtension: 'svg',
+      compile: async function(inputContent) {
+        let result = MathJax.tex2svg(inputContent);
+        return async (data) => { return MathJax.startup.adaptor.innerHTML(result) };
+      }
+    });
 
     eleventyConfig.addTemplateFormats('scss');
     eleventyConfig.addExtension('scss', {
