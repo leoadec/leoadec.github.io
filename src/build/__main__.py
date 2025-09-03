0f705a00144120c215115203ca14bffbc3796201
Leonardo Andreta de Castro <leoadec@leoadec.com>
2025-09-03 23:42:46 +1000

    allow multiple headers

--- src/build/__main__.py
+++ src/build/__main__.py
@@ -24,6 +24,7 @@ if __name__=="__main__":
     with open("config.toml", "rb") as fp:
         config = tomllib.load(fp)
 
+    contents_dir = Path(config["contents_dir"])
     data_dir = Path(config["data_dir"])
 
     template_env = jinja2.Environment(
@@ -35,67 +36,70 @@ if __name__=="__main__":
     cv_template = template_env.get_template("cv.jinja2")
 
     cv = {}
+    data = config["data"]
 
     locations = {}
-    with open(data_dir / config["locations"], "rb") as fp:
-        data = tomllib.load(fp)
-        for key, location in data.items():
+    with open(data_dir / data["locations"], "rb") as fp:
+        loc = tomllib.load(fp)
+        for key, location in loc.items():
             locations[key] = Location.from_dict(location)
 
-    with open(data_dir / config["contact"], "rb") as fp:
+    with open(data_dir / data["contact"], "rb") as fp:
         contact = tomllib.load(fp)
 
-    with open(data_dir / config["authors"], "rb") as fp:
+    with open(data_dir / data["authors"], "rb") as fp:
         authors = {
             key: Author.from_dict(author) for key, author in tomllib.load(fp).items()
         }
 
-    with open(data_dir / config["posters"], "rb") as fp:
+    with open(data_dir / data["posters"], "rb") as fp:
         posters = {
             key: Poster.from_dict(poster, authors) for key, poster in tomllib.load(fp).items()
         }
 
-    with open(data_dir / config["grants"], "rb") as fp:
+    with open(data_dir / data["grants"], "rb") as fp:
         grants = {
             key: Grant.from_dict(grant) for key, grant in tomllib.load(fp).items()
         }
 
-    with open(data_dir / config["theses"], "rb") as fp:
+    with open(data_dir / data["theses"], "rb") as fp:
         theses = {
             key: Thesis.from_dict(thesis, authors) for key, thesis in tomllib.load(fp).items()
         }
 
-    with open(data_dir / config["talks"], "rb") as fp:
+    with open(data_dir / data["talks"], "rb") as fp:
         talks = {
             key: Talk.from_dict(talk, authors) for key, talk in tomllib.load(fp).items()
         }
 
-    with open(data_dir / config["jobs"], "rb") as fp:
+    with open(data_dir / data["jobs"], "rb") as fp:
         cv["jobs"] = [
             Job.from_dict(job, locations).render()
             for job in tomllib.load(fp).values()
         ]
 
-    with open(data_dir / config["degrees"], "rb") as fp:
+    with open(data_dir / data["degrees"], "rb") as fp:
         cv["degrees"] = [
             Degree.from_dict(degree, locations=locations, grants=grants, theses=theses).render()
             for degree in tomllib.load(fp).values()
         ]
 
-    with open(data_dir / config["papers"], "rb") as fp:
+    with open(data_dir / data["papers"], "rb") as fp:
         cv["papers"] = [
             Paper.from_dict(paper, authors).render()
             for paper in tomllib.load(fp).values()
         ]
 
-    with open(data_dir / config["conferences"], "rb") as fp:
+    with open(data_dir / data["conferences"], "rb") as fp:
         cv["conferences"] = [
             Conference.from_dict(conference, locations=locations, posters=posters, talks=talks).render()
             for conference in tomllib.load(fp).values()
         ]
 
-    with open("content/2025_about.md", "r", encoding="utf-8") as fp:
-        content = markdown.markdown(fp.read())
+    for element in config["contents"]:
+        with open(contents_dir / element["file"], "r", encoding="utf-8") as fp:
+            content = f"<h2>{element['title']}</h2>\n\n"
+            content += markdown.markdown(fp.read())
 
     with open("sass/style.scss", "r", encoding="utf-8") as fp:
         scss = fp.read()
